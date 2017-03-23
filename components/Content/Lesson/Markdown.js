d81f85d061f36946e4ae221f189a7cfabd3bc25d
Arunoda Susiripala <arunoda.susiripala@gmail.com>
2017-03-23 15:27:44 +0530

    Make links in the content to open in a new tab.

--- components/Content/Lesson/Markdown.js
+++ components/Content/Lesson/Markdown.js
@@ -1,3 +1,5 @@
+import React from 'react'
+import ReactDom from 'react-dom'
 import marked from 'marked'
 import Highlight from 'react-highlight'
 
@@ -7,66 +9,95 @@ marked.setOptions({
   breaks: true
 })
 
-const Markdown = ({ content }) => (
-  <div className='markdown'>
-    <Highlight innerHTML={true}>
-      {marked(content)}
-    </Highlight>
-    <style jsx global>{`
-      .markdown{
-        font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif;
-        font-size: 16px;
-        line-height: 1.6;
-        max-width: 600px;
-        color: #444;
-        line-height: 25px;
-      }
+export default class Markdown extends React.Component {
+  makeLinksOpenInANewTab() {
+    const dom = ReactDom.findDOMNode(this)
+    const links = dom.getElementsByTagName('a')
+    for(const link of links) {
+      link.setAttribute('target', '_blank')
+    }
+  }
 
-      .markdown h2 {
-        font-size: 20px;
-      }
+  componentDidMount () {
+    this.makeLinksOpenInANewTab()
+  }
 
-      .markdown h3 {
-        font-size: 18px;
-      }
+  componentDidUpdate () {
+    this.makeLinksOpenInANewTab()
+  }
 
-      .markdown blockquote {
-        padding: 10px 20px;
-        margin: 18px 0;
-        border-left: 5px solid #B2CDA9;
-      }
+  render () {
+    const { content } = this.props
+    return (
+      <div className='markdown'>
+        <Highlight innerHTML={true}>
+          {marked(content)}
+        </Highlight>
+        <style jsx global>{`
+          .markdown{
+            font-family: "Helvetica Neue", Helvetica, "Segoe UI", Arial, freesans, sans-serif;
+            font-size: 16px;
+            line-height: 1.6;
+            max-width: 600px;
+            color: #444;
+            line-height: 25px;
+          }
 
-      .markdown blockquote p {
-        margin: 0;
-      }
+          .markdown h2 {
+            font-size: 20px;
+          }
 
-      .markdown pre {
-        font-size: 14px;
-        line-height: 20px;
-        background-color: #f9f9f9;
-        padding: 10px 15px;
-        border: 1px solid #eee;
-        margin: 25px 0;
-      }
+          .markdown h3 {
+            font-size: 18px;
+          }
 
-      .markdown code {
-        font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
-        font-size: 13px;
-        background-color: #f9f9f9;
-        border-radius: 3px;
-        padding: 2px 5px;
-      }
+          .markdown blockquote {
+            padding: 10px 20px;
+            margin: 18px 0;
+            border-left: 5px solid #B2CDA9;
+          }
 
-      .markdown pre code {
-        padding: 0;
-        border-radius: 0;
-      }
+          .markdown blockquote p {
+            margin: 0;
+          }
 
-      .markdown img {
-        max-width: 100%
-      }
-    `}</style>
-  </div>
-)
+          .markdown pre {
+            font-size: 14px;
+            line-height: 20px;
+            background-color: #f9f9f9;
+            padding: 10px 15px;
+            border: 1px solid #eee;
+            margin: 25px 0;
+          }
 
-export default Markdown
+          .markdown code {
+            font-family: "SFMono-Regular", Consolas, "Liberation Mono", Menlo, Courier, monospace;
+            font-size: 13px;
+            background-color: #f9f9f9;
+            border-radius: 3px;
+            padding: 2px 5px;
+          }
+
+          .markdown pre code {
+            padding: 0;
+            border-radius: 0;
+          }
+
+          .markdown img {
+            max-width: 100%
+          }
+
+          .markdown a {
+            text-decoration: none;
+            color: #2196F3;
+            border-bottom: 1px solid #2196F3;
+          }
+
+          .markdown a:hover {
+            opacity: 0.7;
+          }
+        `}</style>
+      </div>
+    )
+  }
+}
